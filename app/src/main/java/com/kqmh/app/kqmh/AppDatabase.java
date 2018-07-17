79d3be3309ecc7daaabee5095c55f7bad061fec9
cromwel <cromwellindy@gmail.com>
2018-07-17 18:41:50 +0300

    added db connector and tables

--- app/src/main/java/com/kqmh/app/kqmh/AppDatabase.java
+++
@@ -1,8 +0,0 @@
-package com.kqmh.app.kqmh;
-
-import com.raizlabs.android.dbflow.annotation.Database;
-
-@Database(version = AppDatabase.VERSION)
-public class AppDatabase {
-    public static final int VERSION = 1;
-}
