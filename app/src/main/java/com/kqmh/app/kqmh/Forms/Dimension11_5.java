7b4340bb7f97a927aae6d0d692288f6dc6c9b118
cromwel <cromwellindy@gmail.com>
2018-08-12 13:37:59 +0300

    update

--- app/src/main/java/com/kqmh/app/kqmh/Forms/Dimension11_5.java
+++ app/src/main/java/com/kqmh/app/kqmh/Forms/Dimension11_5.java
@@ -62,7 +62,7 @@ public class Dimension11_5 extends AppCompatActivity {
 
 
 
-        for(int value=1;value<28;value++){
+        for(int value=1500;value<1522;value++){
             Resources res  = getResources();
             String spinnerParse = String.format(res.getString(R.string.spinner_score),value);
 
