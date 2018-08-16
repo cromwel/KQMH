2205e51d4ff692c6377384571445727106ff3aa9
cromwel <cromwellindy@gmail.com>
2018-08-16 10:09:25 +0300

    update

--- app/src/main/java/com/kqmh/app/kqmh/Activities/ViewFilesActivity.java
+++ app/src/main/java/com/kqmh/app/kqmh/Activities/ViewFilesActivity.java
@@ -1,10 +1,15 @@
 package com.kqmh.app.kqmh.Activities;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.kqmh.app.kqmh.Forms.Dimensions_List;
 import com.kqmh.app.kqmh.R;
+import com.kqmh.app.kqmh.SessionManager;
 
 public class ViewFilesActivity extends AppCompatActivity {
     private ProgressDialog progressDialog;
@@ -18,5 +23,31 @@ public class ViewFilesActivity extends AppCompatActivity {
         progressDialog.setTitle("Fetching Scores");
         progressDialog.setCancelable(false);
 
+        Button complete = findViewById(R.id.bt_start_assessment);
+
+        complete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submit();
+            }
+        });
+
     }
+
+    public void submit() {
+        try{
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        new SessionManager(getBaseContext()).setLoggedIn(true);
+        Intent intent = new Intent(getBaseContext(), Dimensions_List.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK
+                | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 }
